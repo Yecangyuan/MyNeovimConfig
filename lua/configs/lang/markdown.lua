ddefd46affaecc1b9d48e98e02f192fe7b766bb5
Yecangyuan <1826552766@qq.com>
2026-03-19 13:59:22 +0800

    feat: 添加 nvim 内 markdown 预览
    - iamcco/markdown-preview.nvim: 浏览器预览 (<leader>mp)
    - render-markdown.nvim: nvim 内渲染 (<leader>mr)

--- lua/configs/lang/markdown.lua
+++ lua/configs/lang/markdown.lua
@@ -1,42 +1,41 @@
 -- Markdown 预览插件
 return {
-  -- 主流选择：iamcco/markdown-preview.nvim
-  -- 使用浏览器预览，效果最好，社区最流行
+  -- 1. 浏览器预览（效果最好，最完整）
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
-      -- 配置选项
-      vim.g.mkdp_auto_start = 0        -- 不自动打开
-      vim.g.mkdp_auto_close = 1        -- 切换 buffer 时自动关闭
-      vim.g.mkdp_refresh_slow = 0      -- 实时刷新
-      vim.g.mkdp_command_for_global = 0 -- 只对 markdown 文件启用
-      vim.g.mkdp_open_to_the_world = 0 -- 只在本地打开
-      vim.g.mkdp_open_ip = ''
-      vim.g.mkdp_browser = ''          -- 使用默认浏览器
-      vim.g.mkdp_echo_preview_url = 0
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_refresh_slow = 0
       vim.g.mkdp_page_title = '「${name}」'
     end,
     keys = {
-      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", mode = "n", desc = "Markdown Preview - Toggle" },
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", mode = "n", desc = "Markdown Preview - Browser" },
     },
     ft = { "markdown" },
   },
 
-  -- 备选：渲染 markdown 到 nvim 内（不需要浏览器）
-  -- 如果你不想用浏览器预览，可以启用这个
+  -- 2. nvim 内预览（渲染效果，不打开浏览器）
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    enabled = false,  -- 默认禁用，需要时启用
+    enabled = true,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
-    ft = { "markdown", "codecompanion" },
+    ft = { "markdown" },
     opts = {
-      -- 在 nvim 内渲染 markdown，不打开浏览器
+      -- 在 nvim 内渲染 markdown
+      render_modes = true,  -- 所有模式下都渲染
+      anti_conceal = {
+        enabled = true,  -- 允许在光标行显示隐藏字符
+      },
+    },
+    keys = {
+      { "<leader>mr", "<cmd>RenderMarkdown toggle<CR>", mode = "n", desc = "Markdown Preview - Nvim Toggle" },
     },
   },
 }
